9f406b64557e545823a39341e943083a22a07a9e
successJohn <successojohn@gmail.com>
2021-11-15 13:09:36 -0800

    summation

--- add.js
+++
@@ -1,6 +0,0 @@
-function getSum(n){
-    return n(n+1)/2
-}
-
-let trial = getSum(2);
-console.log(trial);
---
+++ summation.js
@@ -0,0 +1,9 @@
+/*
+Write a program that finds the summation of every number from 1 to num. 
+The number will always be a positive integer greater than 0.
+*/
+
+function summation(num){
+    return num * ( (num +1 ) /2);
+}
+
